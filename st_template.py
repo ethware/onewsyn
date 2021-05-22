91be378f49d446acefa1c6f3fa27825b5473fde8
Carlos <carloswlc@foxmail.com>
2021-05-22 09:19:35 +0800

    Add files via upload
    添加排除路径功能；
    变更文件命名方式，便于import；
    添加配置文件工作方式；

---
+++ st_template.py
@@ -0,0 +1,15 @@
+from onewsyn_0_0_4 import tempDir
+
+sambaWork = "//username:password@hostname/Sharedfolder/"
+sambaHome = "//username:password@hostname/Sharedfolder/"
+
+ruleout = [
+    '/Users/USERNAME/Pictures/Photos Library.photoslibrary',
+    tempDir,
+]
+
+workloads = [
+    ["/Users/username/WorkFolder", "WorkFolder", sambaHome],
+    ["/Users/username/WorkFolder", "WorkFolder", sambaWork],
+    ["/Users/username/WorkFolder", "/Volumes/ExternalDrive/WorkFolder"],
+]
